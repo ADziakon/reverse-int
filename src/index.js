819c2ba93080cd9e1e9f5fbe9acf69cf81a7b2d6
ADziakon <a.dziakanchuk@gmail.com>
2023-01-08 01:21:03 +0300

    feat: add reverse functiuon code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let str, str2 ="";
+
+    str = String(Math.abs(n));
+    
+    if (str.length > 1){
+      for (i=str.length-1; i>=0; i--){
+          str2 = str2 + str[i];
+      }
+    }else{
+        return str;
+    }
+    return str2;
 }
